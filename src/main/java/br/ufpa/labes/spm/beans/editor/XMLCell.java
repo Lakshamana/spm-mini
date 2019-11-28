f8ad48e10a59663861eeb87c5979acd44abebe8a
Lakshamana <guitrompa1@gmail.com>
2019-11-28 14:18:46 -0300

    add x & y sse-event-intended coordinates

--- src/main/java/br/ufpa/labes/spm/beans/editor/XMLCell.java
+++ src/main/java/br/ufpa/labes/spm/beans/editor/XMLCell.java
@@ -10,6 +10,8 @@ public class XMLCell {
   private String style;
   private Object sourceNode;
   private Object targetNode;
+  private int x;
+  private int y;
 
   public static final String NORMAL = "Normal",
       DECOMPOSED = "Decomposed",
@@ -181,6 +183,12 @@ public class XMLCell {
         + ", targetNode='"
         + getTargetNode()
         + "'"
+        + ", x='"
+        + getX()
+        + "'"
+        + ", y='"
+        + getY()
+        + "'"
         + "}";
   }
 
@@ -203,5 +211,21 @@ public class XMLCell {
   @Override
   public int hashCode() {
     return Objects.hash(nodeType, label, objectId, isEdge, style, sourceNode, targetNode);
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
   }
 }
